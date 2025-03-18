d6348cbc7d1c472f303cad5e49a6029dda1e9238
GBacellari <gbacellari@beder.edu.al>
2025-03-18 10:51:48 +0100

    initial commit

---
+++ seminar2/bin/seminar2.dart
@@ -0,0 +1,5 @@
+import 'package:seminar2/seminar2.dart' as seminar2;
+
+void main(List<String> arguments) {
+  print('Hello world: ${seminar2.calculate()}!');
+}
---
+++ seminar2/test/seminar2_test.dart
@@ -0,0 +1,8 @@
+import 'package:seminar2/seminar2.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
